1c4e4b2f128ac815864051be5af412997d04ea9c
RENFERD <woron20088f@mail.ru>
2025-02-03 16:04:05 +0500

    'put-delete_requests'

--- src/routers/article.py
+++ src/routers/article.py
@@ -1,4 +1,5 @@
 from datetime import date
+from turtle import title
 from typing import Annotated
 from fastapi import HTTPException, Path, APIRouter
 from pydantic import BaseModel, Field
@@ -22,7 +23,7 @@ async def get_article(id: Annotated[int, Path(title="ID статьи", ge=0)], d
     given_error("article not found", result, 404)
     return result
 
-#Добваление статьи
+#Добавление статьи
 @router.post("/add")
 async def addArticle(article: ArticleBase, db: db_dependence):
     db_article = Article(
@@ -35,3 +36,26 @@ async def addArticle(article: ArticleBase, db: db_dependence):
     db.add(db_article)
     db.commit()
     db.refresh((db_article))
+
+@router.put('/update/{id}')
+async def update_article(id: Annotated[int, Path(title='id статьи', ge=0)], article: ArticleBase, db: db_dependence):
+    db_article = db.query(Article).filter(Article.pk_article_id == id).first()
+    given_error('Статья не найдена', db_article, 404)
+
+    db_article.title = article.title
+    db_article.body = article.body
+    db_article.publish_date = article.publish_date
+
+    db.commit()
+    db.refresh(db_article)
+
+    return db_article
+
+@router.delete('/delete/{id}')
+async def delete_article(id: Annotated[int, Path(title="id статьи", ge=0)], db: db_dependence):
+    db_article = db.query(Article).filter(Article.pk_article_id == id).first()
+    given_error('Статья не найдена', db_article, 404)
+    db.delete(db_article)
+    db.commit()
+
+    return {'Сообщение': 'Статья удалена'}
--- src/routers/category.py
+++ src/routers/category.py
@@ -1,7 +1,10 @@
 from datetime import datetime, date
+from operator import ge
 from os import error
+from turtle import title
 from typing import Annotated
-from fastapi import APIRouter, Query
+from anyio import Path
+from fastapi import APIRouter, Query, Path
 from pydantic import BaseModel, Field, field_validator
 from src.models import Category
 from src.database import db_dependence
@@ -30,3 +33,23 @@ async def add_category(category: CategoryBase, db: db_dependence):
     db.refresh(db_category)
     return db_category
 
+@router.put("/update/{id}")
+async def update_category(id: Annotated[int, Path(title="id категории", ge=0)], category: CategoryBase, db:db_dependence):
+    db_category = db.query(Category).filter(Category.category_id == id).first()
+    given_error("Категория не найдена", db_category, 404)
+    db_category.category_name = category.category_name
+
+    db.commit()
+    db.refresh(db_category)
+
+    return db_category
+
+@router.delete("/delete/{id}")
+async def delete_category(id: Annotated[int, Path(title='id категории', ge=0)], db: db_dependence):
+    db_category = db.query(Category).filter(Category.category_id == id).first()
+    given_error("Категория не найдена", db_category, 404)
+
+    db.delete(db_category)
+    db.commit()
+
+    return {'Сообщение': 'Категория удалена'}
